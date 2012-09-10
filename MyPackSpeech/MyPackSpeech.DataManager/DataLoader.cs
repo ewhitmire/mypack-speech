099999ce20d66fb51f2046f46b0584b582a521a2
bkrichards <brichards@gmail.com>
2012-09-09 22:37:24 -0400

    Create MyPackSpeech solution

---
+++ MyPackSpeech/MyPackSpeech.DataManager/DataLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace MyPackSpeech.DataManager
+{
+   public static class DataLoader
+   {
+      /// <summary>
+      /// Loads data. 
+      /// </summary>
+      /// <param name="window">The window to parent dialogs on.</param>
+      public static void Load(Window window)
+      {
+      }
+   }
+}
