b06746eaf723e74d207aa7f52ddca4996cea9fa9
Brian Richards <brichards@gmail.com>
2012-10-29 21:37:40 -0400

    Implement Undo for Add/Remove

--- MyPackSpeech/MyPackSpeech.SpeechRecognition/Actions/RemoveAction.cs
+++ MyPackSpeech/MyPackSpeech.SpeechRecognition/Actions/RemoveAction.cs
@@ -8,40 +8,40 @@ using MyPackSpeech.DataManager.Data;
 namespace MyPackSpeech.SpeechRecognition.Actions
 {
 	class RemoveAction : IAction
-   {
-      public Student Student { get; private set; }
-	  public ScheduledCourse Course { get; private set; }
-      private SemanticValue semantics = null;
+	{
+		public Student Student { get; private set; }
+		public ScheduledCourse Course { get; private set; }
+		private SemanticValue semantics = null;
 
-      public void Inform(SemanticValue sem, Student student)
-      {
-         Student = student;
-         semantics = sem;
-      }
-      public bool Perform()
-      {
-         List<Slots> missing = ActionManager.ValidateCourse(semantics);
-
-         if (missing.Count > 0)
-         {
-            ActionManager.Instance.PromptForMissing(semantics, missing);
-            return false;
-         }
-
-        Course = CourseConstructor.ContructScheduledCourse(semantics);
-		if (Course != null)
+		public void Inform(SemanticValue sem, Student student)
 		{
-			Student.RemoveCourse(Course);
+			Student = student;
+			semantics = sem;
 		}
-         return true;
-      }    
+		public bool Perform()
+		{
+			List<Slots> missing = ActionManager.ValidateCourse(semantics);
 
-      public void Undo()
-      {
-		  if (Course != null)
-		  {
-			  Student.AddCourse(Course);
-		  }
-      }
-   }
+			if (missing.Count > 0)
+			{
+				ActionManager.Instance.PromptForMissing(semantics, missing);
+				return false;
+			}
+
+			Course = CourseConstructor.ContructScheduledCourse(semantics);
+			if (Course != null)
+			{
+				Student.RemoveCourse(Course);
+			}
+			return true;
+		}
+
+		public void Undo()
+		{
+			if (Course != null)
+			{
+				Student.AddCourse(Course);
+			}
+		}
+	}
 }
