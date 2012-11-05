de5bc3204999d1b773cc368be382aab3c2c237ca
Eric Whitmire <ewhitmire91@gmail.com>
2012-11-05 11:04:20 -0500

    cleaned up indents and dead code in mainwindow, removed test project

--- MyPackSpeech/MyPackSpeech/MainWindow.xaml.cs
+++ MyPackSpeech/MyPackSpeech/MainWindow.xaml.cs
@@ -1,24 +1,24 @@
-﻿   using System;
-   using System.Collections.Generic;
-   using System.Linq;
-   using System.Text;
-   using System.Windows;
-   using System.Windows.Controls;
-   using System.Windows.Data;
-   using System.Windows.Documents;
-   using System.Windows.Input;
-   using System.Windows.Media;
-   using System.Windows.Media.Imaging;
-   using System.Windows.Navigation;
-   using System.Windows.Shapes;
-   using Microsoft.Win32;
-   using MyPackSpeech.SpeechRecognition;
-   using MyPackSpeech.DataManager;
-   using MyPackSpeech.DataManager.Data;
-   using System.Collections.ObjectModel;
-   using System.Speech.Recognition;
-   using System.Speech.Synthesis;
-   using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using MyPackSpeech.SpeechRecognition;
+using MyPackSpeech.DataManager;
+using MyPackSpeech.DataManager.Data;
+using System.Collections.ObjectModel;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
 
 namespace MyPackSpeech
 {
@@ -59,7 +59,6 @@ namespace MyPackSpeech
 
          bookmarked.Remove(course);
          showBookmarks();
-
       }
 
       public void showBookmarks()
@@ -80,7 +79,6 @@ namespace MyPackSpeech
          infoBox.Text = "" + course.Dept.Name + "(" + course.DeptAbv + ")" + " " + course.Number + "\n" +
              course.Description;
       }
-
 
       private void Load_Click(object sender, RoutedEventArgs e)
       {
@@ -108,19 +106,6 @@ namespace MyPackSpeech
          if (dlg.ShowDialog(this).GetValueOrDefault(false))
             return dlg.FileName;
          return string.Empty;
-      }
-     
-      private void showReqs()
-      {
-      }
-      
-      private void showReqs_Click(object sender, RoutedEventArgs e)
-      {
-         showReqs();
-      }
-
-      private void showCourses_Click(object sender, RoutedEventArgs e)
-      {
       }
 
       private void isSpeechOn_Checked(object sender, RoutedEventArgs e)
