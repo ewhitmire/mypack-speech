853b975e48a6bc97e7c059a1326dc3b6246cc6bf
Eric Whitmire <ewhitmire91@gmail.com>
2012-11-10 21:45:25 -0500

    adding UnknownAction

---
+++ MyPackSpeech/MyPackSpeech.SpeechRecognition/Actions/UnknownAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPackSpeech.SpeechRecognition.Actions
+{
+   class UnknownAction : BaseAction
+   {
+      public override bool Perform()
+      {
+         if (CourseConstructor.ContainsCourseData(Semantics).Count == 0)
+         {
+            String courseName = BaseAction.MakeCourseNameForSpeech(Semantics);
+            RecoManager.Instance.Say("What would you like to do with " + courseName);
+         }
+         else
+         {
+            RecoManager.Instance.Say("I'm not sure what you are trying to do.");
+         }
+         return false;
+      }
+
+      public override void Undo()
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
