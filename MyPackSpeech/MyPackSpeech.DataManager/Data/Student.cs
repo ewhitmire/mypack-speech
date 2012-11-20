bedab69b7edfd3818b27c253146aa505deeea74a
Prairie Rose <prgoodwi@ncsu.edu>
2012-11-20 00:06:44 -0500

    fix requirements perf issue

--- MyPackSpeech/MyPackSpeech.DataManager/Data/Student.cs
+++ MyPackSpeech/MyPackSpeech.DataManager/Data/Student.cs
@@ -56,6 +56,7 @@ namespace MyPackSpeech.DataManager.Data
             if (req.CourseRequirement.Matches(course.Course))
             {
                req.Fulfillment = course;
+               break;
             }
          }
          OnScheduleChanged();
