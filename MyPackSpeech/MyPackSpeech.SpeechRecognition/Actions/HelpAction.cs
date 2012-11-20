0d5934ee2ae4ee7b143ccbacea0cefffa12ddbff
bkrichards <brichards@gmail.com>
2012-11-19 23:25:24 -0500

    Help Action

---
+++ MyPackSpeech/MyPackSpeech.SpeechRecognition/Actions/HelpAction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPackSpeech.SpeechRecognition.Actions
+{
+   public class HelpAction : BaseAction
+   {
+      public override bool Perform()
+      {
+         ActionManager.Instance.ShowHelp();
+         return false;
+      }
+
+      public override void Undo()
+      {
+         
+      }
+   }
+}
